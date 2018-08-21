bf812129bfa7a027ffd5c5fb833c0cc34744cb33
Santiago <slloretsantiago27@gmail.com>
2018-08-21 13:01:13 -0300

    Test de bote valor 1

--- battleship/test_board.py
+++ battleship/test_board.py
@@ -19,6 +19,24 @@ class test_board(unittest.TestCase):
 		]
 		self.assertEqual(result, board_expected)
 
+	def test_insert(self):
+		board = Board()
+		board.set_boat(0, 0, 1)
+		board_expected = [
+			[1,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+		]
+		result = board.get_board()
+		self.assertEqual(result, board_expected)
+
 
 if __name__ == '__main__':
 	unittest.main()
