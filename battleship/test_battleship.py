ef283c8c119aada17656e8a96ed39565d3e7af0d
Santiago <slloretsantiago27@gmail.com>
2018-08-21 12:06:53 -0300

    Primer test de tablero

---
+++ battleship/test_battleship.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestBattleship(unittest.TestCase):
+	def test_hit(self):
+		posicion = [0,0]
+		resultado = shoot(posicion)
+		self.assertEqual(resultado, "Hit")
+
+if __name__ == "__main__":
+	unittest.main()
