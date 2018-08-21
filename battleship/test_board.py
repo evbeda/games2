b419933aacc6dbc94b5479f9a119879a1c078105
Santiago <slloretsantiago27@gmail.com>
2018-08-21 12:06:53 -0300

    Primer test de tablero

---
+++ battleship/test_board.py
@@ -0,0 +1,24 @@
+import unittest
+from battleship import Board
+
+class test_board(unittest.TestCase):
+	def test_inicial(self):
+		board = Board()
+		result = board.get_board()
+		board_expected = [
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+		]
+		self.assertEqual(result, board_expected)
+
+
+if __name__ == '__main__':
+	unittest.main()
