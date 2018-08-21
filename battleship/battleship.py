4865d7628dd34aa270cde986d6a4b3ad718ec031
Santiago <slloretsantiago27@gmail.com>
2018-08-21 14:46:38 -0300

    Fix pep8 Battleship

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -1,26 +1,26 @@
 
 
 class Board(object):
-	def __init__(self):
-		self.board = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-		]
+    def __init__(self):
+        self.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
 
-	def get_board(self):
-		return self.board
+    def get_board(self):
+        return self.board
 
-	def set_boat(self,row,column,value):
-		if (column >= 0 and column < 10) and (row >= 0 and row < 10):
-			self.board [row][column] = value
-			return True
-		else:
-			return False
+    def set_boat(self, row, column, value):
+        if (column >= 0 and column < 10) and (row >= 0 and row < 10):
+            self.board[row][column] = value
+            return True
+        else:
+            return False
