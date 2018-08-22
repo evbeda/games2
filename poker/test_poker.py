9a0f1cbe1fe8ee6ed05181656f8e1c8e6f980e89
Santiago <slloretsantiago27@gmail.com>
2018-08-22 14:31:09 -0300

    Apply test doble par and trio

--- poker/test_poker.py
+++ poker/test_poker.py
@@ -7,20 +7,31 @@ class PokerTest(unittest.TestCase):
         # setup
         expected = True
         # test
-        result = encontrarEscaleraReal(["Ah", "Kh", "Qh", "Jh", "Th"])
+        result = encontrarEscaleraReal(['Ah', 'Kh', 'Qh', 'Jh', 'Th'])
         # assert
         self.assertEqual(result, expected)
 
     def test_pares(self):
         # setup
-        expected = ["3"]
+        expected = ['3']
         # test
-        result = encontrarPares(["Kh", "8d", "3c", "3d", "2s"])
+        result = encontrarPares(['Kh', '8d', '3c', '3d', '2s'])
         # assert
         self.assertEqual(result, expected)
 
+    def test_trio(self):
+        expected = ['K']
+        result = encontrarTrio(['Kh', 'Kd', 'Ks', '8h', '10s'])
+        self.assertEqual(result, expected)
 
-#master
+    def test_doblepar(self):
+        expected = ['Q', '8']
+        result = encontrarPares(['Qd', 'Qh', '8d', '8s', '4s'])
+        self.assertEqual(result, expected)
+
+
+
+# master
 # def definirMano(self):
 #     # setup
 
