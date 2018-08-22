d2c2afd7dad289ebc06b02e3f32fbd70c4a925be
Rulo180 <mvallesacc@gmail.com>
2018-08-22 14:45:05 -0300

    Add play a card feature on Truco.

--- truco/player.py
+++ truco/player.py
@@ -1,5 +1,10 @@
 class Player():
     def __init__(self, name):
         self.name = name
-        self.cards = []
+        self.hiddenCards = []
         self.score = []
+        self.playedCards = []
+    
+    def play_card(self, index):
+        playedCard = self.hiddenCards.pop(index)
+        self.playedCards.append(playedCard)
