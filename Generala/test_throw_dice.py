ccf88996f587486a1f1036db4b53144e1c337e4c
biancasozzi <biancasozzi@gmail.com>
2018-08-22 10:36:21 -0300

    Correcion Formato PEP8

--- Generala/test_throw_dice.py
+++ Generala/test_throw_dice.py
@@ -1,10 +1,12 @@
 import unittest
 from throwDice import throwDice
 
-class TestThrowDice(unittest.TestCase):
-	def test_throw(self):
-		throwResult = throwDice(1)
-		self.assertTrue(1 <= throwResult[0] <= 6)
 
-if __name__=='__main__':
-	unittest.main()
+class TestThrowDice(unittest.TestCase):
+    def test_throw(self):
+        throwResult = throwDice(1)
+        self.assertTrue(1 <= throwResult[0] <= 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- Generala/throwDice.py
+++ Generala/throwDice.py
@@ -1,7 +1,8 @@
 import random
 
+
 def throwDice(num):
-	results=[]
-	for number in range(0,num):
-		results.append(random.randint(1,6))
-	return results
+    results = []
+    for number in range(0, num):
+        results.append(random.randint(1, 6))
+    return results
