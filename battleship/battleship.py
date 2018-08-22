ce0e24550994f45475070135cbc86308edfafa4d
arielpozo <arielpozo@eventbrite.com>
2018-08-22 11:54:28 -0300

    Ships insertion in vertical or horizontal position

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -18,9 +18,15 @@ class Board(object):
     def get_board(self):
         return self.board
 
-    def set_boat(self, row, column, value):
+    def set_boat(self, row, column, value, orientation):
         if (column >= 0 and column < 10) and (row >= 0 and row < 10):
-            self.board[row][column] = value
-            return True
+            if (orientation == "horizontal") and ((value + column) <= 10):
+                for index in range(0, value):
+                    self.board[row][column + index] = value
+                return True
+            elif (orientation == "vertical") and ((value + row) <= 10):
+                for index in range(0, value):
+                    self.board[row + index][column] = value
+                return True
         else:
             return False
