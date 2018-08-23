2e3e78050b0c774190ccd7dea46759335bf41ea5
francob <betombino@gmail.com>
2018-08-23 15:14:12 -0300

    Add manosjerarquias Poker

--- poker/poker.py
+++ poker/poker.py
@@ -1,40 +1,68 @@
 from collections import defaultdict
 
 
-def encontrarEscaleraReal(cards):
-    initialSuit = cards[0][1:]
+def ordenar_cartas_numeros(cards):
+    # Obtiene cartas con la pinta y devuelve un array con los numeros ordenados
+    numbers = []
     for card in cards:
-        suit = card[1:]
-        value = card[:1]
-        if (
-            value == "A" or
-            value == "K" or
-            value == "Q" or
-            value == "J" or
-            value == "T"
-        ) and suit == initialSuit:
-            continue
+        if card[0] == 'A':
+            numbers.append(1)
+        elif card[0] == 'T':
+            numbers.append(10)
+        elif card[0] == 'J':
+            numbers.append(11)
+        elif card[0] == 'Q':
+            numbers.append(12)
+        elif card[0] == 'K':
+            numbers.append(13)
         else:
-            return False
-    return True
+            numbers.append(int(card[0]))
+    numbers.sort()
+    return numbers
+
+
+def get_value(card):
+    if card[0] == 'A':
+        return 1
+    elif card[0] == 'T':
+        return 10
+    elif card[0] == 'J':
+        return 11
+    elif card[0] == 'Q':
+        return 12
+    elif card[0] == 'K':
+        return 13
+    else:
+        return int(card[0])
+
+
+def encontrarEscaleraReal(cards):
+    numbers = ordenar_cartas_numeros(cards)
+    # Escalera con AS,10,J,Q,K
+    if numbers == [1, 10, 11, 12, 13] and encontrar_color(cards):
+        return True
+    else:
+        return False
 
 
 def encontrarPares(cards):
     values = defaultdict(int)
+    print (values)
     for card in cards:
-        value = card[:1]
+        value = card[0]
         values[value] = values[value] + 1
     result = []
     for key in values:
         if (values[key] == 2):
             result.append(key)
+    print (values)
     return result
 
 
 def encontrarTrio(cards):
     values = defaultdict(int)
     for card in cards:
-        value = card[:1]
+        value = card[0]
         values[value] = values[value] + 1
     result = []
     for key in values:
@@ -42,5 +70,60 @@ def encontrarTrio(cards):
             result.append(key)
     return result
 
-#def encontrarDoblepar(cards):
- #   values = defaultdict(int)
+
+def encontrar_iguales(cards):
+    cardsDictionary = defaultdict(int)
+    for card in cards:
+        cardNumber = card[0]
+        cardsDictionary[cardNumber] += 1
+    result = {
+        'poker': [],
+        'trio': [],
+        'par': [],
+    }
+    for cardKey in cardsDictionary:
+        if(cardsDictionary[cardKey] == 4):
+            result['poker'].append(cardKey)
+            break
+        if(cardsDictionary[cardKey] == 3):
+            result['trio'].append(cardKey)
+            break
+        if(cardsDictionary[cardKey] == 2):
+            result['par'].append(cardKey)
+    return result
+
+
+def encontrar_color(cards):
+    color = cards[0][1]
+    for card in cards:
+        if card[1] != color:
+            return False
+    return True
+
+
+def encontrar_escalera(cards):
+    numbers = ordenar_cartas_numeros(cards)
+    # Escalera con AS,10,J,Q,K
+    if numbers == [1, 10, 11, 12, 13]:
+        return True
+    for i in range(len(numbers) - 1):
+        if (numbers[i + 1] - numbers[i]) != 1:
+            return False
+    return True
+
+
+def encontrar_escalera_color(cards):
+    if encontrar_color(cards) and encontrar_escalera(cards):
+        return True
+    else:
+        return False
+
+
+def encontrar_cartas_pintas(cards):
+    numbers = ordenar_cartas_numeros(cards)
+    orderCards = []
+    for number in numbers:
+        for card in cards:
+            if(get_value(card[0]) == number):
+                orderCards.append(card)
+    return orderCards
--- poker/test_poker.py
+++ poker/test_poker.py
@@ -4,39 +4,105 @@ from .poker import *
 
 class PokerTest(unittest.TestCase):
     def test_escaleraReal(self):
-        # setup
-        expected = True
         # test
         result = encontrarEscaleraReal(['Ah', 'Kh', 'Qh', 'Jh', 'Th'])
         # assert
-        self.assertEqual(result, expected)
+        self.assertTrue(result)
 
     def test_pares(self):
         # setup
-        expected = ['3']
+        expected = {
+            'poker': [],
+            'trio': [],
+            'par': ['3'],
+        }
         # test
-        result = encontrarPares(['Kh', '8d', '3c', '3d', '2s'])
+        result = encontrar_iguales(['Kh', '8d', '3c', '3d', '2s'])
         # assert
         self.assertEqual(result, expected)
 
     def test_trio(self):
-        expected = ['K']
-        result = encontrarTrio(['Kh', 'Kd', 'Ks', '8h', '10s'])
+        expected = {
+            'poker': [],
+            'trio': ['K'],
+            'par': [],
+        }
+        result = encontrar_iguales(['Kh', 'Kd', 'Ks', '8h', 'Ts'])
         self.assertEqual(result, expected)
 
     def test_doblepar(self):
-        expected = ['Q', '8']
-        result = encontrarPares(['Qd', 'Qh', '8d', '8s', '4s'])
+        expected = {
+            'poker': [],
+            'trio': [],
+            'par': ['Q', '8'],
+        }
+        result = encontrar_iguales(['Qd', 'Qh', '8d', '8s', '4s'])
+        self.assertEqual(result, expected)
+
+    def test_poker(self):
+        expected = {
+            'poker': ['7'],
+            'trio': [],
+            'par': [],
+        }
+        result = encontrar_iguales(['7d', '7h', '7d', '7s', '4s'])
+        self.assertEqual(result, expected)
+
+    def test_color_true(self):
+        result = encontrar_color(['Ad', '2d', '3d', '7d', '4d'])
+        self.assertTrue(result)
+
+    def test_color_false(self):
+        result = encontrar_color(['Ah', '2d', '3d', '7d', '4d'])
+        self.assertFalse(result)
+
+    def test_full_house(self):
+        expected = {
+            'poker': [],
+            'trio': ['7'],
+            'par': ['2'],
+        }
+        result = encontrar_iguales(['2d', '2h', '7d', '7s', '7s'])
+        self.assertEqual(result, expected)
+
+    def test_escalera_true(self):
+        result = encontrar_escalera(['2h', 'Ad', '3d', '4d', '5d'])
+        self.assertTrue(result)
+
+    def test_escalera_false(self):
+        result = encontrar_escalera(['8h', 'Ad', '3d', '4d', '5d'])
+        self.assertFalse(result)
+
+    def test_escalera_as(self):
+        result = encontrar_escalera(['Th', 'Ad', 'Kd', 'Jd', 'Qd'])
+        self.assertTrue(result)
+
+    def test_escalera_color_true(self):
+        result = encontrar_escalera_color(['2h', 'Ah', '3h', '4h', '5h'])
+        self.assertTrue(result)
+
+    def test_escalera_color_false_not_color(self):
+        result = encontrar_escalera_color(['2d', 'Ah', '3h', '4h', '5h'])
+        self.assertFalse(result)
+
+    def test_escalera_color_false_not_escalera(self):
+        result = encontrar_escalera_color(['8h', 'Ah', '3h', '4h', '5h'])
+        self.assertFalse(result)
+
+    def test_escalera_color_false_not_any(self):
+        result = encontrar_escalera_color(['8d', 'Ah', '3h', '4h', '5h'])
+        self.assertFalse(result)
+
+    def test_carta_cartas_pintas(self):
+        expected = ['Ad', '3h', '4h', '5h', '8d']
+        result = encontrar_cartas_pintas(['8d', 'Ad', '3h', '4h', '5h'])
+        self.assertEqual(result, expected)
+
+    def test_ordenar_cartas_numeros(self):
+        expected = [1, 3, 4, 5, 8]
+        result = ordenar_cartas_numeros(['8d', 'Ad', '3h', '4h', '5h'])
         self.assertEqual(result, expected)
 
 
-
-# master
-# def definirMano(self):
-#     # setup
-
-#     # test
-
-#     # assert
 if __name__ == "__main__":
     unittest.main()
