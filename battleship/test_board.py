2e0aefc184d106a16492843af7de79edf4facdc3
biancasozzi <biancasozzi@gmail.com>
2018-08-23 12:26:01 -0300

    Sunked Boat verified

--- battleship/test_board.py
+++ battleship/test_board.py
@@ -22,7 +22,7 @@ class test_board(unittest.TestCase):
 
     def test_insert(self):
         board = Board()
-        board.set_boat(2, 3, 1, "horizontal")
+        board.set_boat(2, 3, 1, 1, "horizontal")
         board_expected = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -40,9 +40,9 @@ class test_board(unittest.TestCase):
 
     def test_insert_three_horizontal_ship(self):
         board = Board()
-        board.set_boat(0, 7, 3, "horizontal")
+        board.set_boat(0, 7, 31, 3, "horizontal")
         board_expected = [
-            [0, 0, 0, 0, 0, 0, 0, 3, 3, 3],
+            [0, 0, 0, 0, 0, 0, 0, 31, 31, 31],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -58,7 +58,7 @@ class test_board(unittest.TestCase):
 
     def test_insert_five_vertifcal_ship(self):
         board = Board()
-        board.set_boat(3, 4, 5, "vertical")
+        board.set_boat(3, 4, 5, 5, "vertical")
         board_expected = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -76,41 +76,30 @@ class test_board(unittest.TestCase):
 
     def test_error_position(self):
         board = Board()
-        result = board.set_boat(10, 10, 1, "horizontal")
+        result = board.set_boat(10, 10, 1, 1, "horizontal")
         self.assertEqual(result, False)
 
     def test_error_position_four_horizontal_ship(self):
         board = Board()
-        result = board.set_boat(8, 7, 4, "horizontal")
+        result = board.set_boat(8, 7, 4, 4, "horizontal")
         self.assertFalse(result)
 
     def test_error_position_five_vertical_ship(self):
         board = Board()
-        result = board.set_boat(7, 7, 5, "vertical")
+        result = board.set_boat(7, 7, 5, 5, "vertical")
         self.assertFalse(result)
 
     def test_error_position_already_has_ship_vertical(self):
         board = Board()
-        board.set_boat(3, 3, 4, "vertical")
-        result = board.set_boat(4, 3, 2, "vertical")
+        board.set_boat(3, 3, 4, 4, "vertical")
+        result = board.set_boat(4, 3, 2, 2, "vertical")
         self.assertFalse(result)
 
     def test_error_position_already_has_ship_horizontal(self):
         board = Board()
-        board.set_boat(2, 2, 4, "horizontal")
-        result = board.set_boat(2, 3, 2, "vertical")
+        board.set_boat(2, 2, 4, 4, "horizontal")
+        result = board.set_boat(2, 3, 2, 2,"vertical")
         self.assertFalse(result)
-
-    def test_shoot_water(self):
-        board = Board()
-        result = board.shoot(3, 3)
-        self.assertEqual(result, "water")
-
-    def test_shoot_hit(self):
-        board = Board()
-        board.set_boat(1, 1, 2, "horizontal")
-        result = board.shoot(1, 1)
-        self.assertEqual(result, "hit")
 
 
 if __name__ == '__main__':
