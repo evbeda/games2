79086608f155bfc1f6bcfd3e3b078fe84cc56a24
biancasozzi <biancasozzi@gmail.com>
2018-08-24 15:17:44 -0300

    deal mocked but verify not finished

--- poker/card.py
+++ poker/card.py
@@ -2,3 +2,9 @@ class Card(object):
     def __init__(self, value, color):
         self.value = value
         self.color = color
+
+    def __repr__(self):
+        return '{}{}'.format(
+            self.value,
+            self.color,
+        )
