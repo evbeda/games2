ecd0ec2cedcc922e2e63ff652493879e321f8d07
francob <betombino@gmail.com>
2018-08-27 15:28:16 -0300

    generala integration

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from guess_number_game.guess_number_game import GuessNumberGame
+from Generala.game import Game as GeneralaGame
 
 
 class Game(object):
@@ -7,6 +8,7 @@ class Game(object):
         super(Game, self).__init__()
         self.games = [
             GuessNumberGame,
+            GeneralaGame,
         ]
 
     def output(self, text):
@@ -41,10 +43,7 @@ class Game(object):
                 expecting_str,
             ))
             try:
-                input_args = [
-                    int(simple_arg)
-                    for simple_arg in inputs.split(' ')
-                ]
+                input_args = inputs.split(' ')
                 if len(input_args) == self.active_game.input_args:
                     break
                 else:
@@ -70,23 +69,23 @@ class Game(object):
                 break
             if game_selection < len(self.games):
                 self.active_game = self.games[game_selection]()
-                try:
-                    while (
-                        (
-                            hasattr(self.active_game, 'playing') and
-                            self.active_game.playing
-                        ) or (
-                            hasattr(self.active_game, 'is_playing') and
-                            self.active_game.is_playing
-                        )
-                    ):
-                        self.output(self.active_game.board)
-                        game_input = self.get_turn_input(
-                            self.active_game.next_turn(),
-                        )
-                        self.output(self.active_game.play(*game_input))
-                except Exception as e:
-                    self.output('Sorry... {}'.format(e))
+                # try:
+                while (
+                    (
+                        hasattr(self.active_game, 'playing') and
+                        self.active_game.playing
+                    ) or (
+                        hasattr(self.active_game, 'is_playing') and
+                        self.active_game.is_playing
+                    )
+                ):
+                    self.output(self.active_game.board)
+                    game_input = self.get_turn_input(
+                        self.active_game.next_turn(),
+                    )
+                    self.output(self.active_game.play(*game_input))
+                # except Exception as e:
+                #     self.output('Sorry... {}'.format(e))
 
 
 if __name__ == '__main__':
