e239160c72fa25634062c66a80c1bfaeca2e0849
arielpozo <arielpozo@eventbrite.com>
2018-08-27 11:29:56 -0300

    Who Wins method

--- Blackjack/deck.py
+++ Blackjack/deck.py
@@ -4,17 +4,18 @@ import random
 class Deck():
     def __init__(self, values, suits):
         self.values = values
-        self.suits = suits 
+        self.suits = suits
         self.cards = []
         for value in values:
             for suit in suits:
                 self.cards.append(value + suit)
+        self.shuffle()
 
     def shuffle(self):
-        random.shuffle(self.cards)    
-    
+        random.shuffle(self.cards)
+
     def deal(self, amount):
         result = []
         for i in range(amount):
-            result.append(self.cards.pop())        
+            result.append(self.cards.pop())
         return result
