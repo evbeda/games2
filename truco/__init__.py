201042f743ae30e1de4eba3bc1521e01cad66add
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-08-29 13:50:04 -0300

    move dic to init

--- truco/__init__.py
+++ truco/__init__.py
@@ -2,3 +2,15 @@ ESPADA = 'espada'
 BASTO = 'basto'
 ORO = 'oro'
 COPA = 'copa'
+envido_combinations = [
+    ['Envido', 2, 1],
+    ['Real Envido', 3, 1],
+    ['Falta Envido', 0, 1],
+    ['Envido', 'Envido', 4, 2],
+    ['Envido', 'Real Envido', 5, 2],
+    ['Envido', 'Falta Envido', 0, 2],
+    ['Envido', 'Envido', 'Real Envido', 7, 4],
+    ['Envido', 'Envido', 'Falta Envido', 0, 4],
+    ['Envido', 'Real Envido', 'Falta Envido', 0, 5],
+    ['Envido', 'Envido', 'Real Envido', 'Falta Envido', 0, 7],
+]
--- truco/deck.py
+++ truco/deck.py
@@ -7,22 +7,31 @@ from . import ESPADA, BASTO, ORO, COPA
 class Deck(object):
 
     def __init__(self):
-        self.hierarchical_deck = [
-            Carta(ESPADA, 1), Carta(BASTO, 1), Carta(ESPADA, 7),
-            Carta(ORO, 7), Carta(ESPADA, 3), Carta(BASTO, 3), Carta(ORO, 3), Carta(COPA, 3),
-            Carta(ESPADA, 2), Carta(BASTO, 2), Carta(ORO, 2), Carta(COPA, 2),
+        self.ordered_deck = [
+            Carta(ESPADA, 1), Carta(BASTO, 1),
+            Carta(ESPADA, 7), Carta(ORO, 7),
+            Carta(ESPADA, 3), Carta(BASTO, 3),
+            Carta(ORO, 3), Carta(COPA, 3),
+            Carta(ESPADA, 2), Carta(BASTO, 2),
+            Carta(ORO, 2), Carta(COPA, 2),
             Carta(ORO, 1), Carta(COPA, 1),
-            Carta(ESPADA, 12), Carta(BASTO, 12), Carta(ORO, 12), Carta(COPA, 12),
-            Carta(ESPADA, 11), Carta(BASTO, 11), Carta(ORO, 11), Carta(COPA, 11),
-            Carta(ESPADA, 10), Carta(BASTO, 10), Carta(ORO, 10), Carta(COPA, 10),
+            Carta(ESPADA, 12), Carta(BASTO, 12),
+            Carta(ORO, 12), Carta(COPA, 12),
+            Carta(ESPADA, 11), Carta(BASTO, 11),
+            Carta(ORO, 11), Carta(COPA, 11),
+            Carta(ESPADA, 10), Carta(BASTO, 10),
+            Carta(ORO, 10), Carta(COPA, 10),
             Carta(BASTO, 7), Carta(COPA, 7),
-            Carta(ESPADA, 6), Carta(BASTO, 6), Carta(ORO, 6), Carta(COPA, 6),
-            Carta(ESPADA, 5), Carta(BASTO, 5), Carta(ORO, 5), Carta(COPA, 5),
-            Carta(ESPADA, 4), Carta(BASTO, 4), Carta(ORO, 4), Carta(COPA, 4)
+            Carta(ESPADA, 6), Carta(BASTO, 6),
+            Carta(ORO, 6), Carta(COPA, 6),
+            Carta(ESPADA, 5), Carta(BASTO, 5),
+            Carta(ORO, 5), Carta(COPA, 5),
+            Carta(ESPADA, 4), Carta(BASTO, 4),
+            Carta(ORO, 4), Carta(COPA, 4)
         ]
 
     def get_card(self):
-        indice = random.randint(0, len(self.hierarchical_deck) - 1)
-        aux = self.hierarchical_deck[indice]
-        del self.hierarchical_deck[indice]
+        indice = random.randint(0, len(self.ordered_deck) - 1)
+        aux = self.ordered_deck[indice]
+        del self.ordered_deck[indice]
         return aux
